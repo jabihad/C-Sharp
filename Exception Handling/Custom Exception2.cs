d91b4da58357c3089e2b30efdae3d7318a8babdd
Abu Bakar Siddique <jabihad@gmail.com>
2020-04-14 17:30:29 +0600

    Create Custom Exception2.cs

---
+++ Exception Handling/Custom Exception2.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Runtime.Serialization;
+namespace ExceptionHandling
+{
+    public class UserAlreadyLoggedInException : Exception
+    {
+        public UserAlreadyLoggedInException(string message)
+            : base(message)
+        {
+        }
+        public UserAlreadyLoggedInException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+        public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserAlreadyLoggedInException("User Already logged in");
+            }
+            catch (UserAlreadyLoggedInException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
